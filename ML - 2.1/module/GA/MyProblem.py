c326235f4b42d21d5f5e90bb361a5da218d54e53
X1AOX1A <1764400017@e.gzhu.edu.cn>
2020-02-25 16:31:43 +0800

    Add path for Ai-Studio

--- ML - 2.1/module/GA/MyProblem.py
+++ ML - 2.1/module/GA/MyProblem.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import cross_val_score
 
 import sys
 sys.path.append('/Users/apple/Documents/ML_Project/ML - 2.1/module')
+# sys.path.append('/home/aistudio/work/module')
 from utils import *
 from ngboost.learners import *
 from sklearn.metrics import mean_squared_error
@@ -18,10 +19,10 @@ class MyProblem(ea.Problem): # 继承Problem父类
         name = 'MyProblem' # 初始化name（函数名称，可以随意设置）
         M = 1 # 初始化M（目标维数）
         maxormins = [1] # 初始化maxormins（目标最小最大化标记列表，1：最小化该目标；-1：最大化该目标）
-        Dim = 4 # 初始化Dim（决策变量维数）
-        varTypes = [1, 1, 0, 0] # 初始化varTypes（决策变量的类型，元素为0表示对应的变量是连续的；1表示是离散的）
-        lb = [1, 1, 0, 0.5] # 决策变量下界
-        ub = [10000, 2000, 1, 1] # 决策变量上界
+        Dim = 5 # 初始化Dim（决策变量维数）[n_readout, n_components, damping, weight_scaling, alpha]
+        varTypes = [1, 1, 0, 0, 0] # 初始化varTypes（决策变量的类型，元素为0表示对应的变量是连续的；1表示是离散的）
+        lb = [1, 1, 0, 0.5, 0] # 决策变量下界
+        ub = [10000, 2000, 1, 1, 1] # 决策变量上界
         lbin = [0] * Dim # 决策变量下边界（0表示不包含该变量的下边界，1表示包含）
         ubin = [1] * Dim # 决策变量上边界（0表示不包含该变量的上边界，1表示包含）
         # 调用父类构造方法完成实例化
@@ -51,7 +52,7 @@ class MyProblem(ea.Problem): # 继承Problem父类
             result.wait()
             pop.ObjV = np.array(result.get())
     
-    def test(self, n_readout, n_components, damping, weight_scaling): # 代入优化后的参数对测试集进行检验
+    def test(self, n_readout, n_components, damping, weight_scaling, alpha): # 代入优化后的参数对测试集进行检验
         # 读取测试集数据
         X_train, X_test, Y_train, Y_test = get_data(hour_num=0, transform='sin+cos',
                                             drop_time=True, scale=True)
@@ -65,7 +66,7 @@ class MyProblem(ea.Problem): # 继承Problem父类
                 n_components=n_components,
                 damping = damping,
                 weight_scaling = weight_scaling,
-                alpha=0.01).fit(self.data, self.dataTarget) # 创建分类器对象并用训练集的数据拟合分类器模型
+                alpha=alpha).fit(self.data, self.dataTarget) # 创建分类器对象并用训练集的数据拟合分类器模型
 
         # esn = esn_linear_svr_learner(
         #         n_readout=n_readout,
@@ -91,13 +92,14 @@ def subAimFunc(args):
     n_components = int(Vars[i, 1])
     damping = Vars[i, 2]
     weight_scaling = Vars[i, 3]
+    alpha = Vars[i, 4]
  
     esn = esn_ridge_learner(
             n_readout=n_readout,
             n_components=n_components,
             damping = damping,
             weight_scaling = weight_scaling,
-            alpha=0.01) # 创建分类器对象并用训练集的数据拟合分类器模型
+            alpha=alpha) # 创建分类器对象并用训练集的数据拟合分类器模型
 
     # esn = esn_linear_svr_learner(
     #             n_readout=n_readout,
@@ -109,6 +111,6 @@ def subAimFunc(args):
     #             max_iter=10000) # 创建分类器对象并用训练集的数据拟合分类器模型
     
     excute_time() # 显示执行 CV 的次数
-    scores = -cross_val_score(esn, data, dataTarget, cv=10, scoring='neg_mean_squared_error') # 计算交叉验证的得分
+    scores = -cross_val_score(esn, data, dataTarget, cv=4, scoring='neg_mean_squared_error') # 计算交叉验证的得分
     ObjV_i = [scores.mean()] # 把交叉验证的平均得分作为目标函数值
     return ObjV_i
