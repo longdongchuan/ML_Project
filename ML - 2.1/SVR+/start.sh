70919a33cd34865889506e4d1b2b761951c70544
X1AOX1A <1764400017@e.gzhu.edu.cn>
2020-03-02 00:41:25 +0800

    Add shell for Liunx

--- ML - 2.1/SVR+/start.sh
+++ ML - 2.1/SVR+/start.sh
@@ -1,5 +1,10 @@
-# ! /bin/bash
-cd /root/Documents/ML_Project/ML\ -\ 2.1/SVR+
-# cd ~/Documents/ML_Project/ML\ -\ 2.1/SVR+
+cd ～/Documents/ML_Project/ML\ -\ 2.1
+# cd ~/Documents/ML_Project/ML\ -\ 2.1
+git pull
+echo git pull Done
+
+cd SVR+
+echo start time: $(date)
 python3 main.py > result.log &
 echo Done!!!!!!!!!!!
+echo Finsh time: $(date)
