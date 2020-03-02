4f4a51b46d6b19e571bcbec7c0d38d7e586d80ae
Ali_linux <1764400017@e.gzhu.edu.cn>
2020-03-02 11:34:59 +0800

    test for SVR+

--- ML - 2.1/SVR+/start.sh
+++ ML - 2.1/SVR+/start.sh
@@ -1,10 +1,10 @@
-cd ～/Documents/ML_Project/ML\ -\ 2.1
+# cd ～/Documents/ML_Project/ML\ -\ 2.1
 # cd ~/Documents/ML_Project/ML\ -\ 2.1
 git pull
 echo git pull Done
 
 cd SVR+
 echo start time: $(date)
-python3 main.py > result.log &
+python3 test.py > result.log &
 echo Done!!!!!!!!!!!
 echo Finsh time: $(date)
