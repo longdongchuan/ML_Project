65d08da3f78406ac7e754e4965031d7d7e9816b3
X1AOX1A <1764400017@e.gzhu.edu.cn>
2020-03-09 00:31:42 +0800

    Add y as X_star

--- ML - 2.1/SVR+/test.py
+++ ML - 2.1/SVR+/test.py
@@ -12,10 +12,14 @@ Parameters['gamma_rbf_corSpace'] = 10
 Parameters['epsilon'] = 0.1
 Parameters['tol'] = 1e-4
 
-X_train, X_test, Y_train, Y_test = get_data(hour_num=1, transform='sin+cos')
+X_train, X_test, Y_train, Y_test = get_data(
+    hour_num=1, transform='sin+cos',
+    train_index=[6426,10427],
+    test_index=[14389,15390])
 X_train = np.array(X_train)
 X_test = np.array(X_test)
-Y_train = np.array(Y_train)
-Y_test = np.array(Y_test)
+Y_train = np.array(Y_train).reshape(-1, 1)
+Y_test = np.array(Y_test).reshape(-1, 1)
 
-svr_plus(X_train, X_test, Y_train, Y_test, X_train, Parameters)
+X_star=Y_train
+svr_plus(X_train, X_test, Y_train, Y_test, X_star, Parameters)
